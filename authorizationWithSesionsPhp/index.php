5edc554f056a9779a40efa970c6388a418acf2b7
Abdulaziz <abdulazizfaactory@gmail.com>
2021-04-06 12:17:44 +0500

    authorization and registration pages are almost done

---
+++ authorizationWithSesionsPhp/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="main.css">
+    <title>Authorization and Registration</title>
+</head>
+<body>
+    <!--Authorization form-->
+    <form action="" method="post">
+        <label>Login</label>
+        <input type="text"  placeholder="Enter your login">
+
+        <label>Password</label>
+        <input type="password" placeholder="Enter your password">
+
+        <button>Log In</button>
+        <p>
+            You have an account ? - <a href="register.php">Register</a>
+        </p>
+        
+    </form>
+</body>
+</html>
---
+++ authorizationWithSesionsPhp/register.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="main.css">
+    <title>Authorization and Registration</title>
+</head>
+<body>
+    <!--Authorization form-->
+    <form action="" method="post">
+        <label>Full name</label>
+        <input type="text"  placeholder="Enter your full name">
+
+        <label>login</label>
+        <input type="text" placeholder="Enter your login">
+
+        <label>E-mail</label>
+        <input type="email" placeholder="Enter your e-mail">
+
+       <label>Profile Photo</label>
+        <input type="file" placeholder="Enter your file">
+
+        <label>Password</label>
+        <input type="password" placeholder="Enter your password">
+
+        <label>Confirm Password</label>
+        <input type="password" placeholder="Enter your password">
+
+        <button>Log In</button>
+        <p>
+            You don't have an account ? - <a href="index.php">Authorize</a>
+        </p>
+        
+    </form>
+</body>
+</html>
