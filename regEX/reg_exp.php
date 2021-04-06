3a572234655e451581a04d04b9c61992a799f1f7
Abdulaziz <abdulazizfaactory@gmail.com>
2021-04-06 10:43:19 +0500

    Learning authorization with php sessions

---
+++ regEX/reg_exp.php
@@ -0,0 +1,27 @@
+<?php
+//Lookahead Example: Simple Password Validation
+//1. The password must have between six and ten word characters \w
+//2. It must include at least one lowercase character [a-z]
+//3. It must include at least three uppercase characters [A-Z]
+//4. It must include at least one digit \d
+// Condition 1
+// \A\w{6,10}\z // with string 6 to 10 WORD chars
+// 'A' and 'z' are anchors like ^ and $
+
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
